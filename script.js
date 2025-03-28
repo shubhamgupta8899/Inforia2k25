8b3cb3177180d977ed9c8db6765e0fa3d5e339fe
shubhamGupta <shubhamgupta889909@gmail.com>
2025-03-29 01:12:21 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+// document.getElementById('menu_bar').addEventListener('click', function () {
+//     document.querySelector('.dropMenu').classList.toggle('showDropMenu');
+// });
+
+// const scroll = new SmoothScroll('a[href*="#"]', {
+//     speed: 1000, // Animation speed in milliseconds
+//     easing: 'easeInOutCubic', // Easing function
+//     offset: 100, // Scroll offset in pixels
+// });
+
+
+// function toggleAnswer(id) {
+//     // Hide all answers
+//     document.querySelectorAll('.faq-answer').forEach(function(answer) {
+//         answer.classList.remove('show');
+//     });
+//     // Show the answer for the clicked question
+//     document.getElementById('answer' + id).classList.add('show');
+// }
+// Mobile Menu Toggle
 const mobileMenuBtn = document.getElementById('mobileMenuBtn');
 const mobileMenu = document.getElementById('mobileMenu');
 const mobileMenuClose = document.getElementById('mobileMenuClose');
